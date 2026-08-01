8595ee3391404dced5cec99b3a7d046e6fe86b36
Patrick Shepherd <shepherd.berea@gmail.com>
2026-08-01 15:11:43 -0400

    added base class for nodes

--- safyr/node.py
+++ safyr/node.py
@@ -1,106 +1,111 @@
 # definitions of AST nodes
 
 
-class NumberNode:
-    def __init__(self, tok):
-        self.tok = tok
+class Node:
+    """Base class for all visitable nodes.
+    
+    Parameters
+    ----------
+    pos_start
+    pos_end
+    """
+    def __init__(self, pos_start, pos_end):
+        self.pos_start = pos_start
+        self.pos_end   = pos_end
 
-        self.pos_start = self.tok.pos_start
-        self.pos_end = self.tok.pos_end
+
+class NumberNode(Node):
+    """
+    """
+    def __init__(self, tok):
+        super().__init__(tok.pos_start, tok.pos_end)
+        self.tok = tok
 
     def __repr__(self):
         return f'{self.tok}'
 
 
-class StringNode:
+class StringNode(Node):
     def __init__(self, tok):
+        super().__init__(tok.pos_start, tok.pos_end)
         self.tok = tok
-
-        self.pos_start = self.tok.pos_start
-        self.pos_end = self.tok.pos_end
 
     def __repr__(self):
         return f'"{self.tok}"'
 
 
-class CapsuleNode:
+class CapsuleNode(Node):
     def __init__(self, element_nodes, pos_start, pos_end):
+        super().__init__(pos_start, pos_end)
         self.elements = element_nodes
 
-        self.pos_start = pos_start
-        self.pos_end = pos_end
 
-
-class ListNode:
+class ListNode(Node):
     def __init__(self, element_nodes, pos_start, pos_end):
+        super().__init__(pos_start, pos_end)
         self.elements = element_nodes
 
-        self.pos_start = pos_start
-        self.pos_end = pos_end
 
-
-class MapNode:
+class MapNode(Node):
     def __init__(self, elements, pos_start, pos_end):
+        super().__init__(pos_start, pos_end)
         self.elements = elements
 
-        self.pos_start = pos_start
-        self.pos_end = pos_end
 
-
-class UseNode:
+class UseNode(Node):
     def __init__(self, fname):
+        super().__init__(fname.pos_start, fname.pos_end)
         self.fname = fname
-
-        self.pos_start = self.fname.pos_start
-        self.pos_end = self.fname.pos_end
 
     def __repr__(self):
         return f'<{self.fname}>'
 
 
-class DeleteNode:
+class DeleteNode(Node):
     def __init__(self, name):
+        super().__init__(name.pos_start, name.pos_end)
         self.name = name
-
-        self.pos_start = self.name.pos_start
-        self.pos_end = self.name.pos_end
 
     def __repr__(self):
         return f'<delete {self.name}>'
 
 
-class VarAccessNode:
+class VarAccessNode(Node):
     def __init__(self, var_name_tok):
+        super().__init__(var_name_tok.pos_start, var_name_tok.pos_end)
         self.var_name_tok = var_name_tok
-
-        self.pos_start = self.var_name_tok.pos_start
-        self.pos_end = self.var_name_tok.pos_end
 
     def __repr__(self):
         return f'{self.var_name_tok}'
 
 
-class ReferenceAccessNode:
+class ReferenceAccessNode(Node):
     def __init__(self, head):
+        super().__init__(head.pos_start, head.pos_end)
         self.head = head
-
-        self.pos_start = self.head.value_node.pos_start
-        self.pos_end = self.head.value_node.pos_end
 
     def __repr__(self):
         return f'{self.head}'
 
 
-class ReferenceAssignNode:
-    def __init__(self, target_node, op_tok, value_node):
+class ReferenceAssignNode(Node):
+    def __init__(self,
+                 target_node,
+                 op_tok,
+                 value_node):
+        super().__init__(target_node.pos_start, value_node.pos_end)
         self.target_node = target_node
         self.op_tok = op_tok
         self.value_node = value_node
 
 
-class VarAssignNode:
-    def __init__(self, var_name_tok, op_tok, value_node,
+class VarAssignNode(Node):
+    def __init__(self,
+                 var_name_tok,
+                 op_tok,
+                 value_node,
                  constvar=False, globalvar=False, statictype=None):
+        super().__init__(var_name_tok.pos_start, var_name_tok.pos_end)
         self.var_name_tok = var_name_tok
         self.op_tok = op_tok
         self.value_node = value_node
@@ -108,49 +113,54 @@ class VarAssignNode:
         self.globalvar = globalvar
         self.statictype = statictype
 
-        self.pos_start = self.var_name_tok.pos_start
-        self.pos_end = self.value_node.pos_end
-
     def __repr__(self):
         return f'({self.var_name_tok} {self.op_tok} {self.value_node})'
 
 
-class BinOpNode:
-    def __init__(self, left_node, op_tok, right_node):
+class BinOpNode(Node):
+    def __init__(self,
+                 left_node,
+                 op_tok,
+                 right_node):
+        super().__init__(left_node.pos_start, right_node.pos_end)
         self.left_node = left_node
         self.op_tok = op_tok
         self.right_node = right_node
-
-        self.pos_start = self.left_node.pos_start
-        self.pos_end = self.right_node.pos_end
 
     def __repr__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
 
 
-class UnaryOpNode:
-    def __init__(self, op_tok, node):
+class UnaryOpNode(Node):
+    def __init__(self,
+                 op_tok,
+                 node):
+        super().__init__(op_tok.pos_start, node.pos_end)
         self.op_tok = op_tok
         self.node = node
-
-        self.pos_start = self.op_tok.pos_start
-        self.pos_end = node.pos_end
 
     def __repr__(self):
         return f'({self.op_tok}, {self.node})'
 
 
-class IfNode:
-    def __init__(self, cases, else_case):
+class IfNode(Node):
+    def __init__(self,
+                 cases,
+                 else_case):
+        super().__init__(cases[0][0].pos_start, (else_case or cases[len(cases) - 1])[0].pos_end)
         self.cases = cases
         self.else_case = else_case
 
-        self.pos_start = self.cases[0][0].pos_start
-        self.pos_end = (self.else_case or self.cases[len(self.cases) - 1])[0].pos_end
 
-
-class ForNode:
-    def __init__(self, var_name_tok, start_value_node, end_value_node, step_value_node, body_node, should_return_null):
+class ForNode(Node):
+    def __init__(self,
+                 var_name_tok,
+                 start_value_node,
+                 end_value_node,
+                 step_value_node,
+                 body_node,
+                 should_return_null):
+        super().__init__(var_name_tok.pos_start, body_node.pos_end)
         self.var_name_tok = var_name_tok
         self.start_value_node = start_value_node
         self.end_value_node = end_value_node
@@ -158,119 +168,131 @@ class ForNode:
         self.body_node = body_node
         self.should_return_null = should_return_null
 
-        self.pos_start = self.var_name_tok.pos_start
-        self.pos_end = self.body_node.pos_end
 
-
-class ForEachNode:
-    def __init__(self, var_name_tok, container_node, body_node, should_return_null):
+class ForEachNode(Node):
+    def __init__(self,
+                 var_name_tok,
+                 container_node,
+                 body_node,
+                 should_return_null):
+        super().__init__(var_name_tok.pos_start, body_node.pos_end)
         self.var_name_tok = var_name_tok
         self.container_node = container_node
         self.body_node = body_node
         self.should_return_null = should_return_null
 
-        self.pos_start = self.var_name_tok.pos_start
-        self.pos_end = self.body_node.pos_end
 
-
-class WhileNode:
-    def __init__(self, condition_node, body_node, should_return_null):
-        self.condition_node = condition_node
-        self.body_node = body_node
-        self.should_return_null = should_return_null
-
-        self.pos_start = self.condition_node.pos_start
-        self.pos_end = self.body_node.pos_end
-
-
-class WhenNode:
-    def __init__(self, condition_node, body_node, should_return_null):
+class WhenNode(Node):
+    def __init__(self,
+                 condition_node,
+                 body_node,
+                 should_return_null):
+        super().__init__(condition_node.pos_start, body_node.pos_end)
         self.condition_node = condition_node
         self.target = condition_node.left_node.var_name_tok.value
 
         self.body_node = body_node
         self.should_return_null = should_return_null
 
-        self.pos_start = self.condition_node.pos_start
-        self.pos_end = self.body_node.pos_end
 
-
-class DeferNode:
-    def __init__(self, body_node, should_return_null):
+class WhileNode(Node):
+    def __init__(self,
+                 condition_node,
+                 body_node,
+                 should_return_null):
+        super().__init__(condition_node.pos_start, body_node.pos_end)
+        self.condition_node = condition_node
         self.body_node = body_node
         self.should_return_null = should_return_null
 
-        self.pos_start = self.body_node.pos_start
-        self.pos_end = self.body_node.pos_end
+
+class DeferNode(Node):
+    def __init__(self,
+                 body_node,
+                 should_return_null):
+        super().__init__(body_node.pos_start, body_node.pos_end)
+        self.body_node = body_node
+        self.should_return_null = should_return_null
 
 
-class ContinueNode:
+class ContinueNode(Node):
     def __init__(self, pos_start, pos_end):
-        self.pos_start = pos_start
-        self.pos_end = pos_end
+        super().__init__(pos_start, pos_end)
 
 
-class BreakNode:
+class BreakNode(Node):
     def __init__(self, pos_start, pos_end):
-        self.pos_start = pos_start
-        self.pos_end = pos_end
+        super().__init__(pos_start, pos_end)
 
 
-class OnceNode:
+class OnceNode(Node):
     def __init__(self, pos_start, pos_end):
-        self.pos_start = pos_start
-        self.pos_end = pos_end
+        super().__init__(pos_start, pos_end)
 
 
-class ReturnNode:
-    def __init__(self, return_node, pos_start, pos_end):
+class ReturnNode(Node):
+    def __init__(self,
+                 return_node,
+                 pos_start,
+                 pos_end):
+        super().__init__(pos_start, pos_end)
         self.return_node = return_node
-        self.pos_start = pos_start
-        self.pos_end = pos_end
+        
 
 
-class InterfaceDefinitionNode:
-    def __init__(self, var_name_tok, body_node, auto_return):
+class InterfaceDefinitionNode(Node):
+    def __init__(self,
+                 var_name_tok,
+                 body_node,
+                 auto_return):
+        super().__init__(var_name_tok.pos_start, body_node.pos_end)
         self.var_name_tok = var_name_tok
         self.body_node = body_node
         self.auto_return = auto_return
 
-        self.pos_start = self.var_name_tok.pos_start
-        self.pos_end = self.body_node.pos_end
 
-
-class FunctionDefinitionNode:
-    def __init__(self, var_name_tok, arg_name_toks, body_node, auto_return):
+class FunctionDefinitionNode(Node):
+    def __init__(self,
+                 var_name_tok,
+                 arg_name_toks,
+                 body_node,
+                 auto_return):
+        if var_name_tok:
+            start = var_name_tok.pos_start
+        elif arg_name_toks:
+            start = arg_name_toks[0].pos_start
+        else:
+            start = body_node.pos_start
+        super().__init__(start, body_node.pos_end)
         self.var_name_tok = var_name_tok
         self.arg_name_toks = arg_name_toks
         self.body_node = body_node
         self.auto_return = auto_return
 
-        if self.var_name_tok:
-            self.pos_start = self.var_name_tok.pos_start
-        elif len(self.arg_name_toks) > 0:
-            self.pos_start = self.arg_name_toks[0].pos_start
-        else:
-            self.pos_start = self.body_node.pos_start
 
-        self.pos_end = self.body_node.pos_end
-
-
-class CallNode:
-    def __init__(self, node_to_call, arg_nodes):
+class CallNode(Node):
+    def __init__(self,
+                 node_to_call,
+                 arg_nodes):
+        end = arg_nodes[-1].pos_end if arg_nodes else node_to_call.pos_end
+        super().__init__(node_to_call.pos_start, end)
         self.node_to_call = node_to_call
         self.arg_nodes = arg_nodes
 
-        self.pos_start = self.node_to_call.pos_start
 
-        if len(self.arg_nodes) > 0:
-            self.pos_end = self.arg_nodes[len(self.arg_nodes) - 1].pos_end
+class StructDefinitionNode(Node):
+    def __init__(self,
+                 var_name_tok,
+                 arg_name_toks,
+                 body_node,
+                 auto_return):
+        if var_name_tok:
+            start = var_name_tok.pos_start
+        elif arg_name_toks:
+            start = arg_name_toks[0].pos_start
         else:
-            self.pos_end = self.node_to_call.pos_end
-
-
-class StructDefinitionNode:
-    def __init__(self, var_name_tok, arg_name_toks, body_node, auto_return):
+            start = body_node.pos_start
+        super().__init__(start, body_node.pos_end)
         self.var_name_tok = var_name_tok
         self.arg_name_toks = arg_name_toks
         self.body_node = body_node
@@ -278,22 +300,30 @@ class StructDefinitionNode:
 
         self.interfaces = []
 
-        if self.var_name_tok:
-            self.pos_start = self.var_name_tok.pos_start
-        elif len(self.arg_name_toks) > 0:
-            self.pos_start = self.arg_name_toks[0].pos_start
-        else:
-            self.pos_start = self.body_node.pos_start
 
-        self.pos_end = self.body_node.pos_end
+class ErrorHandlerNode(Node):
+    """Organizes error logic branches.
 
+    ::INPUT::
 
-class ErrorHandlerNode:
-    def __init__(self, try_tok, try_node, catch_node, auto_return=True):
+    - try_tok     : Token
+        The token object
+
+    - try_node    : Node
+        Node containing `try` block.
+
+    - catch_node  : Node
+        Node containing `catch` block.
+
+    - auto_return : bool (optional)
+
+    """
+    def __init__(self,
+                 try_tok,
+                 try_node,
+                 catch_node,
+                 auto_return=True):
+        super().__init__(try_tok.pos_start, try_tok.pos_end)
         self.try_node = try_node
         self.catch_node = catch_node
         self.auto_return = auto_return
-
-        self.pos_start = try_tok.pos_start
-
-        self.pos_end = try_tok.pos_end
